33ed7530319ccf149a8cb7b1e5843544f5f00adf
Adams-Ganiwu <110114090+Adams-Ganiwu@users.noreply.github.com>
2022-09-20 13:21:49 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -2,23 +2,19 @@ const express = require("express")
 const dotenv = require("dotenv")
 const connectDB =require("./config/connectDB")
 const userRoute = require("./routes/userRoute")
+const schoolRoute = require("./routes/schoolRoute")
 const morgan = require("morgan")
 
 
-
-
-
 const app = express();
-
 dotenv.config();
 connectDB();
 
 //middlewares
 app.use(express.json())
 app.use("/api/users", userRoute)
-app.use(morgan("dev"))
-
-
+app.use("/api/schools", schoolRoute);
+app.use(morgan("dev"));
 
 
 
